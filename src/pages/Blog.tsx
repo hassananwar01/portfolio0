0503293632b3f562a7947c1ff6231a13e99fe555
hassananwar01 <hassanahwar01@gmail.com>
2024-11-27 19:02:54 +0200

    Add files via upload

---
+++ src/pages/Blog.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import BlogComponent from '../components/Blog';
+
+export default function Blog() {
+  return (
+    <main className="pt-16">
+      <BlogComponent />
+    </main>
+  );
+}
---
+++ src/pages/TeachingPortfolio.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Teaching from '../components/Teaching';
+
+export default function TeachingPortfolio() {
+  return (
+    <main className="pt-16">
+      <Teaching />
+    </main>
+  );
+}
